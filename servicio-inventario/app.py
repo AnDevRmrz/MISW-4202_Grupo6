0bfb85a47a62a4a6ffa474a1cade52dd30fb294d
AlejandroBlanco2001 <nuclearhands2001@gmail.com>
2025-02-19 09:43:40 -0500

    add: health endpoint

--- servicio-inventario/app.py
+++ servicio-inventario/app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status': 'ok'}), 200
+
 @app.route('/product/<int:product_id>', methods=['GET'])
 def get_product_invetory(product_id: int):
     response = {
